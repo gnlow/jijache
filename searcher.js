92511bec136bd535db62b69c2a22ffd15c569a8e
Gnlow <entrydark@gmail.com>
2020-08-17 12:11:02 +0900

    write searchURL function

--- searcher.js
+++ searcher.js
@@ -1,11 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        headless: false,
+        ignoreHTTPSErrors: true,
+    })
     const page = await browser.newPage()
 
     const search = async keyword => {
-        await page.goto('https://google.com')
+        await page.goto("https://google.com")
         await page.evaluate(keyword => {
             const input = document.querySelector("div > div > form > div > div > div > div > div > input")
             input.value = keyword
@@ -14,10 +17,19 @@ const puppeteer = require('puppeteer');
         }, keyword)
         await page.waitForNavigation()
     }
+    const searchURL = async url => {
+        await search(`site:${url} ai 로고 다운로드`)
+        await page.evaluate(() => {
+            document.querySelector(".r > a").click()
+        })
+        await page.waitForNavigation()
+        
+        console.log(page.url())
+    }
 
-    await search("Hello")
+    await searchURL("http://www.gangneung.go.kr")
 
-    await page.screenshot({path: 'example.png'})
+    await page.screenshot({path: "example.png"})
 
     await browser.close()
 })()
